5dd8b4ffe356c6bc27dd5014ae0bda093ca42c72
AJ Tech City <arunjai12456@icloud.com>
2026-06-15 23:05:07 +0100

    Added RBAC associations

--- backend/api/src/database/associations.ts
+++ backend/api/src/database/associations.ts
@@ -1,5 +1,9 @@
 import User from "../modules/users/user.model";
 import Token from "../modules/auth/token.model";
+import Role from "../modules/rbac/role.model";
+import UserRole from "../modules/rbac/UserRole.model";
+import Permission, {PermissionId} from "../modules/rbac/permission.model";
+import RolePermission from "../modules/rbac/RolePermission.model";
 
 export function setupAssociations() {
 
@@ -8,11 +12,28 @@ export function setupAssociations() {
         foreignKey: "user_id",
         as: "tokens"
     });
+    User.belongsToMany(Role,{
+        through: UserRole
+    })
 
     //Token associations
     Token.belongsTo(User, {
         foreignKey: "user_id",
         as: "user"
     });
+
+    //Role associations
+    Role.belongsToMany(User,{
+        through: UserRole
+    })
+    Role.belongsToMany(Permission, {
+        through: RolePermission
+    })
+
+    //Permission Associations
+    Permission.belongsToMany(Role, {
+        through: RolePermission
+    })
+
 
 }
