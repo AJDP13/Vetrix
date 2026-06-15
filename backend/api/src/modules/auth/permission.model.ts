494e83c6bf6483f8763107616e83fb48af68a894
AJ Tech City <arunjai12456@icloud.com>
2026-06-15 22:36:47 +0100

    Create Permission Model and basic PermissionId enum values

---
+++ backend/api/src/modules/auth/permission.model.ts
@@ -0,0 +1,53 @@
+import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import sequelize from "../../config/database";
+
+export enum PermissionId{
+    ROLES_CREATE = "roles.create",
+    ROLES_VIEW = "roles.view",
+    ROLES_EDIT = "roles.edit",
+    ROLES_DELETE = "roles.delete",
+
+    USERS_CREATE = "users.create",
+    USERS_VIEW = "users.view",
+    USERS_EDIT = "users.edit",
+    USERS_DEACTIVATE = "users.deactivate",
+
+    DRUGS_CREATE = "drugs.create",
+    DRUGS_VIEW = "drugs.view",
+    DRUGS_EDIT = "drugs.edit",
+    DRUGS_DELETE = "drugs.delete",
+
+    PRESCRIPTIONS_CREATE = "prescriptions.create",
+    PRESCRIPTIONS_VIEW = "prescriptions.view",
+    PRESCRIPTIONS_EDIT = "prescriptions.edit",
+    PRESCRIPTIONS_DELETE = "prescriptions.delete"
+}
+
+export default class Permission extends Model<InferAttributes<Permission>,InferCreationAttributes<Permission>> {
+    declare id: string;
+    declare description: string;
+
+    declare createdAt: CreationOptional<Date>;
+    declare updatedAt: CreationOptional<Date>;
+}
+
+Permission.init({
+    id:{
+        type: DataTypes.STRING,
+        primaryKey: true
+    },
+
+    description:{
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+},{
+    sequelize,
+    tableName: "vt.permissions",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+});
