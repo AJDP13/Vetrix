ac4af1a1b3c8b58818e8394510ef56b8cdac3f0a
AJ Tech City <arunjai12456@icloud.com>
2026-06-15 22:42:43 +0100

    Crate RolePermission Model

---
+++ backend/api/src/modules/auth/RolePermission.model.ts
@@ -0,0 +1,40 @@
+import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import sequelize from "../../config/database";
+import {PermissionId} from "./permission.model";
+
+export default class RolePermission extends Model<InferAttributes<RolePermission>,InferCreationAttributes<RolePermission>> {
+    declare id: CreationOptional<string>;
+
+    declare role_id: string;
+    declare permission_id: PermissionId;
+
+    declare createdAt: CreationOptional<Date>;
+    declare updatedAt: CreationOptional<Date>;
+}
+
+RolePermission.init({
+    id:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+
+    role_id:{
+        type: DataTypes.UUID,
+        allowNull:false
+    },
+
+    permission_id:{
+        type: DataTypes.UUID,
+        allowNull:false
+    },
+
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+},{
+    sequelize,
+    tableName: "vt.RolePermission",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+});
