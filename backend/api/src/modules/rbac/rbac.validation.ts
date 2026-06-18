6b0a8a1bdc9aa3a9a84d3ac4b24f0e1ea6c6d017
AJ Tech City <arunjai12456@icloud.com>
2026-06-18 11:40:19 +0100

    Improved list validation to include unique values

--- backend/api/src/modules/rbac/rbac.validation.ts
+++ backend/api/src/modules/rbac/rbac.validation.ts
@@ -5,17 +5,17 @@ import {PermissionId} from "./permission.model";
 export const createRoleSchema = Joi.object({
     name: Joi.string().required(),
     description: Joi.string().required(),
-    permissions: Joi.array().items(Joi.string().valid(...Object.values(PermissionId))).required(),
+    permissions: Joi.array().items(Joi.string().valid(...Object.values(PermissionId))).unique().required(),
     priority: Joi.number().positive().required()
 })
 
 export const updateRoleSchema = Joi.object({
     name: Joi.string().optional(),
     description: Joi.string().optional(),
-    permissions: Joi.array().items(Joi.string().valid(...Object.values(PermissionId))).optional(),
+    permissions: Joi.array().items(Joi.string().valid(...Object.values(PermissionId))).unique().optional(),
     priority: Joi.number().positive().optional()
 })
 
 export const updateUserRolesSchema = Joi.object({
-    roles: Joi.array().items(Joi.string().valid(...Object.values(Permissions))).required()
+    roles: Joi.array().items(Joi.string().valid(...Object.values(Permissions))).unique().required()
 })
