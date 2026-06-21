ede6c9ad35bca264be6f63e5b36821e7e19c9c6a
AJ Tech City <arunjai12456@icloud.com>
2026-06-21 01:24:14 +0100

    Added DOB to Pet Model

--- backend/api/src/modules/pets/pet.model.ts
+++ backend/api/src/modules/pets/pet.model.ts
@@ -13,6 +13,7 @@ export default class Pet extends Model<InferAttributes<Pet>,InferCreationAttribu
     declare id: CreationOptional<string>;
 
     declare name: string;
+    declare date_of_birth: Date;
 
     declare created_at: CreationOptional<Date>;
     declare updated_at: CreationOptional<Date>;
@@ -33,6 +34,12 @@ Pet.init({
         unique: false,
         allowNull: false
     },
+
+    date_of_birth:{
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+
     created_at: DataTypes.DATE,
     updated_at: DataTypes.DATE,
     deleted_at: DataTypes.DATE
