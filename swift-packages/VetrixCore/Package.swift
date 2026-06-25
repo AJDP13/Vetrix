a4e2b2184d48bee82742b78d2d31e526e13a6ad6
AJ Tech City <arunjai12456@icloud.com>
2026-06-25 13:59:00 +0100

    Initial Swift Package setup

---
+++ swift-packages/VetrixCore/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VetrixCore",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "VetrixCore",
+            targets: ["VetrixCore"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "VetrixCore"),
+        .testTarget(
+            name: "VetrixCoreTests",
+            dependencies: ["VetrixCore"]
+        ),
+    ]
+)
