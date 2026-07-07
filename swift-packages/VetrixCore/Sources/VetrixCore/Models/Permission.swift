0bb0497103d9c035747f01337147cb3749de8656
AJ Tech City <arunjai12456@icloud.com>
2026-07-08 00:00:18 +0100

    Added full permission enums to swift

--- swift-packages/VetrixCore/Sources/VetrixCore/Models/Permission.swift
+++ swift-packages/VetrixCore/Sources/VetrixCore/Models/Permission.swift
@@ -8,10 +8,37 @@
 import Foundation
 
 public enum PermissionId: String, CaseIterable, Codable, Sendable {
-	case viewUsers = "users.view"
+	case createRoles = "roles.create"
+	case viewRoles = "roles.view"
+	case editRoles = "roles.edit"
+	case deleteRoles = "roles.delete"
+
+	case manageUserRoles = "roles.manage_users"
+
 	case createUsers = "users.create"
+	case viewUsers = "users.view"
 	case editUsers = "users.edit"
-	case archiveUsers = "users.archive"
+	case deactivateUsers = "users.deactivate"
+
+	case createClients = "clients.create"
+	case viewClients = "clients.view"
+	case editClients = "clients.edit"
+	case archiveClients = "clients.archive"
+
+	case createPets = "pets.create"
+	case viewPets = "pets.view"
+	case editPets = "pets.edit"
+	case archivePets = "pets.archive"
+
+	case createDrugs = "drugs.create"
+	case viewDrugs = "drugs.view"
+	case editDrugs = "drugs.edit"
+	case deleteDrugs = "drugs.delete"
+
+	case createPrescriptions = "prescriptions.create"
+	case viewPrescriptions = "prescriptions.view"
+	case editPrescriptions = "prescriptions.edit"
+	case deletePrescriptions = "prescriptions.delete"
 }
 
 public struct Permission: Codable, Sendable{
